c245263466e108444bf5d25c15ce495bd18f7722
NikhilRao21 <suprnick1@gmail.com>
2023-07-23 13:36:20 -0400

    added all 3 push notifs

--- home.js
+++ home.js
@@ -1,14 +1,39 @@
-var time = new Date()
-
 Notification.requestPermission().then(perm => {
-    if (perm === "granted" && time.getHours == 9 && time.getMinutes ==0 && time.getSeconds == 0) {
+   console.log(perm);
+})
+var time = new Date();
+setInterval(function() {
+    var time = Date()
+    return time;
+}, 100);
+setInterval(function() {
+    if (time.getHours() == 9 && time.getMinutes() ==0 && time.getSeconds() == 0) {
         new Notification("From Remind - Good Morning!", {
-            body: "Enter Notifications For the Day! "
+            body: "Good Morning! What do you need to work on today or in the future? Update your Remind! "
 
         })
+        console.log("Push Given. All is well.");
+    }
+    else if (time.getHours() == 11 && time.getMinutes() ==49 && time.getSeconds() ==20) {
+        new Notification("From Remind - Good Afternoon!", {
+            body: "You are halfway through the day! What have you completed? What are you still working on? Update your Remind!"
+
+        })
+        console.log("Push Given. All is well.");
+    }
+    else if (time.getHours() == 19 && time.getMinutes() ==0 && time.getSeconds() == 0) {
+        new Notification("From Remind - Good Evening!", {
+            body: "You are done with the day! How did it go? What have you done? Update your Remind!"
+
+        })
+        console.log("Push Given. All is well.");
+    }
+    else {
+        console.log("No Push Given. Time is " + time);
     }
 
-})
+}, 100);
+
 function newElement(){
     if(document.querySelector('#newtask input').value.length == 0){
         alert("Please enter the name of the task.")
